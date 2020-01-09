fd54767b5461a3ae47c1125ed680276bce8cd85b
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-08 18:32:52 -0700

    prevent flash fixed

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 $(document).ready(function() {
     //my specific API key
     var API = "186f0ba8d0e4fc1b44628ede64658ed5";
-    $("#addCity").on("click", function() {
+    $("#addCity").on("click", function(event) {
+        event.preventDefault()
       var input = $("#input").val();
+        currentWeather(input)
       // clear input box
       $("#input").val("");
-      searchWeather(input);
+console.log("checking input", input);
 
     });
     $(".history").on("click", "li", function() {
@@ -24,18 +26,16 @@ $(document).ready(function() {
     $.ajax({
       type: "GET",
       url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + API,
-      dataType: "json",
-      success: function(data) {
+      dataType: "json"})
+      .then((data) => function() {
         // create history link for this search
+        console.log("checking data", data);
         if (history.indexOf(input) === -1) {
           history.push(input);
           window.localStorage.setItem("history", JSON.stringify(history));
     
           makeRow(input);
         }
-    }
-
-    })
+      })
 }
-
 })
